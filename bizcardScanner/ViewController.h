dc5872443c00950f3b0a3911f48b103edca4ed76
sj442 <sj442@cornell.edu>
2015-04-28 10:02:59 -0400

    Initial Commit

---
+++ bizcardScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bizcardScanner
+//
+//  Created by Sunayna Jain on 4/28/15.
+//  Copyright (c) 2015 Enhatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
